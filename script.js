a9d8ec2e4e705542708d9e64f31dc2f004e0a7f4
Daniel <dwinsness@gmail.com>
2025-01-28 12:15:33 -0500

    Chang scaling for mobile

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ var init = function () {
     if (loaded) return;
     loaded = true;
     var mobile = window.isDevice;
-    var koef = mobile ? 0.5 : 1;
+    var koef = innerWidth < 768 ? 0.7 : 1; // Scale for devices narrower than 768px
     var canvas = document.getElementById('heart');
     var ctx = canvas.getContext('2d');
     var width = canvas.width = koef * innerWidth;
